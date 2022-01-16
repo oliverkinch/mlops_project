fae7af63671e7d26d1cbd15d370111f2f68bff22
Markus Sørensen <s183997@student.dtu.dk>
2022-01-16 15:43:14 +0100

    config + wandb

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,6 +8,9 @@ from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 from transformers import Trainer, AutoModelForSequenceClassification, AutoTokenizer
 from transformers import TrainingArguments
 from datasets import Dataset
+import hydra
+import wandb
+import logging
 
 import random
 
@@ -51,148 +54,138 @@ def compute_metrics(eval_pred):
             'recall': recall
             }
 
-if __name__== "__main__":
+wandb.login()
+@hydra.main(config_path="../../configs", config_name="config.yaml")
 
+def main(config):
     ### use GPU
+    with wandb.init(project=config["model"]["name"], config=dict(config["hyperparameters"])):
+        os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+        torch.cuda.empty_cache()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    torch.cuda.empty_cache()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        mname = config["model"]["name"]
+        if mname not in base_models:
+            exit(0)
+        cwd = os.getcwd().split("mlops_project")[0] + "mlops_project\\"
+        base_model = base_models[mname]
+        data_dir = cwd + config["dirs"]["data"]
 
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("-m", "--model", default="bert", type=str)
-    argparser.add_argument("-s", "--datadir", default='data/processed/', type=str)
-    argparser.add_argument("-p", "--savepath", default="models", type=str)
-    argparser.add_argument("-e", "--epochs", default=1, type=int)
-    argparser.add_argument("-b", "--batch", default=32, type=int)
-    argparser.add_argument("-l", "--maxlength", default=512, type=int)
-    argparser.add_argument("-d", "--devset", default=0.2, type=float)
-    args = argparser.parse_args()
+        epochs = wandb.config["epochs"]
+        devset_ratio = wandb.config["devset_ratio"]
+        assert(0<devset_ratio<1)
+        
+        assert(os.path.exists(cwd + config["dirs"]["models"]))
+        save_dir = os.path.join(cwd + config["dirs"]["models"], base_model["save"])
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
 
-    mname = args.model
-    if mname not in base_models:
-        exit(0)
+        ### DATA PREPROCESSING
 
-    base_model = base_models[mname]
-    data_dir = args.datadir
+        train = torch.load(data_dir + "train.pt")
+        validation = torch.load(data_dir + "validation.pt")
+        test = torch.load(data_dir + "test.pt")
 
-    epochs = args.epochs
-    devset_ratio = args.devset
-    assert(0<devset_ratio<1)
+        labels = ['hate-speech', 'no-hate-speech'] ###
+        labels_str2int = {l:i for i,l in enumerate(labels)}
+        with open(os.path.join(save_dir, 'labels.json'), 'wt') as f:
+            json.dump(labels_str2int, f)
 
-    assert(os.path.exists(args.savepath))
-    save_dir = os.path.join(args.savepath, base_model["save"])
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+        train_data = [t["tweet"].lower() if not base_model['cased'] else t["tweet"] for t in train] ###
+        train_labels = [t["label"] for t in train] ###
 
-    ### DATA PREPROCESSING
+        assert(len(train_data) == len(train_labels))
 
-    train = torch.load(data_dir + "train.pt")
-    validation = torch.load(data_dir + "validation.pt")
-    test = torch.load(data_dir + "test.pt")
+        validation_data = [t["tweet"].lower() if not base_model['cased'] else t["tweet"] for t in validation] ###
+        validation_labels = [t["label"] for t in validation] ###
 
-    # df_train = pd.read_csv(data_dir + 'train.tsv', sep='\t')
-    # df_test = pd.read_csv(data_dir + 'test.tsv', sep='\t')
+        assert(len(validation_data) == len(validation_labels))
 
-    # df_train = pd.read_csv(os.path.join(data_dir, "train.csv"), sep=',', engine='python', encoding='utf-8').dropna() ###
-    # df_test = pd.read_csv(os.path.join(data_dir, "test.csv"), sep=',', engine='python', encoding='utf-8').dropna() ###
+        test_data = [t["tweet"].lower() if not base_model['cased'] else t["tweet"] for t in test] ###
+        test_labels = [t["label"] for t in test] ###
 
-    labels = ['hate-speech', 'no-hate-speech'] ###
-    labels_str2int = {l:i for i,l in enumerate(labels)}
-    with open(os.path.join(save_dir, 'labels.json'), 'wt') as f:
-        json.dump(labels_str2int, f)
+        assert(len(test_data) == len(test_labels))
 
-    train_data = [t["tweet"].lower() if not base_model['cased'] else t["tweet"] for t in train] ###
-    train_labels = [t["label"] for t in train] ###
+        ### LOAD PRE-TRAINED MODEL and TOKENIZER
 
-    assert(len(train_data) == len(train_labels))
+        model_checkpoint = base_model["checkpoint"]
+        tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, do_lower_case=(not base_model['cased']))
+        model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=len(labels))
 
-    validation_data = [t["tweet"].lower() if not base_model['cased'] else t["tweet"] for t in validation] ###
-    validation_labels = [t["label"] for t in validation] ###
+        batch_size = wandb.config["batch_size"]
+        max_length = wandb.config["maxlength"]
 
-    assert(len(validation_data) == len(validation_labels))
+        ### CREATE DATASETS -- TOKENIZE
 
-    test_data = [t["tweet"].lower() if not base_model['cased'] else t["tweet"] for t in test] ###
-    test_labels = [t["label"] for t in test] ###
+        train_dataset = Dataset.from_dict({"text":train_data, "class":train_labels})
+        validation_dataset = Dataset.from_dict({"text":validation_data, "class":validation_labels})
+        test_dataset = Dataset.from_dict({"text":test_data, "class":test_labels})
 
-    assert(len(test_data) == len(test_labels))
+        def preprocess(examples):
+            tokenized_batch = tokenizer(examples['text'], add_special_tokens=True, truncation=True, max_length=max_length)
+            tokenized_batch["labels"] = [label for label in examples["class"]]
+            return tokenized_batch
 
-    ### LOAD PRE-TRAINED MODEL and TOKENIZER
+        train_encodings = train_dataset.map(preprocess, batched=True)
+        dev_encodings = validation_dataset.map(preprocess, batched=True)
+        test_encodings = test_dataset.map(preprocess, batched=True)
 
-    model_checkpoint = base_model["checkpoint"]
-    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, do_lower_case=(not base_model['cased']))
-    model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=len(labels))
+        ### TRAINING ARGUMENTS
 
-    batch_size = args.batch
-    max_length = args.maxlength
+        model = model.to(device)
+        metric_name = wandb.config["metric_name"]
+        
+        t_args = TrainingArguments(
+            save_dir, # directory : where to save the model
+            evaluation_strategy = wandb.config["evaluation_strategy"],
+            save_strategy = wandb.config["save_strategy"],
+            learning_rate = wandb.config["lr"],
+            per_device_train_batch_size = batch_size,
+            per_device_eval_batch_size = wandb.config["per_device_eval_batch_size"],
+            num_train_epochs = epochs,
+            weight_decay = wandb.config["weight_decay"],
+            load_best_model_at_end = wandb.config["load_best_model_at_end"],
+            metric_for_best_model = metric_name,
+        )
 
-    ### CREATE DATASETS -- TOKENIZE
+        ### TRAIN 
 
-    train_dataset = Dataset.from_dict({"text":train_data, "class":train_labels})
-    validation_dataset = Dataset.from_dict({"text":validation_data, "class":validation_labels})
-    test_dataset = Dataset.from_dict({"text":test_data, "class":test_labels})
+        print("#"*20+"\nTRAINING\n")
+        print("model = ", mname)
+        print("epochs = ", epochs)
+        print("batch size =", batch_size)
+        print("max length = ", max_length)
+        print("train data = ", len(train_data))
+        print("dev data = ", len(validation_data), "(", devset_ratio, "% of train )")
+        print('\n')
 
-    def preprocess(examples):
-        tokenized_batch = tokenizer(examples['text'], add_special_tokens=True, truncation=True, max_length=max_length)
-        tokenized_batch["labels"] = [label for label in examples["class"]]
-        return tokenized_batch
+        trainer = Trainer(
+            model,
+            t_args,
+            train_dataset=train_encodings,
+            eval_dataset=dev_encodings,
+            tokenizer=tokenizer,
+            compute_metrics=compute_metrics
+        )
+        wandb.watch(model)
 
-    train_encodings = train_dataset.map(preprocess, batched=True)
-    dev_encodings = validation_dataset.map(preprocess, batched=True)
-    test_encodings = test_dataset.map(preprocess, batched=True)
+        trainer.train()
 
-    ### TRAINING ARGUMENTS
+        # SAVE MODEL
 
-    model = model.to(device)
-    metric_name = "f1"
+        model.save_pretrained(save_dir)
+        tokenizer.save_pretrained(save_dir)
 
-    
-    t_args = TrainingArguments(
-        save_dir, # directory : where to save the model
-        evaluation_strategy = "epoch",
-        save_strategy = "epoch",
-        learning_rate = 2e-5,
-        per_device_train_batch_size = batch_size,
-        per_device_eval_batch_size = 16,
-        num_train_epochs = epochs,
-        weight_decay = 0.01,
-        load_best_model_at_end = True,
-        metric_for_best_model = metric_name,
-    )
+        ### EVALUATE
 
-    ### TRAIN 
+        trainer.evaluate()
 
-    print("#"*20+"\nTRAINING\n")
-    print("model = ", mname)
-    print("epochs = ", epochs)
-    print("batch size =", batch_size)
-    print("max length = ", max_length)
-    print("train data = ", len(train_data))
-    print("dev data = ", len(validation_data), "(", devset_ratio, "% of train )")
-    print('\n')
+        predictions, labels, _ = trainer.predict(test_encodings)
 
-    trainer = Trainer(
-        model,
-        t_args,
-        train_dataset=train_encodings,
-        eval_dataset=dev_encodings,
-        tokenizer=tokenizer,
-        compute_metrics=compute_metrics
-    )
+        preds = predictions.argmax(-1)
+        results = compute_metrics((predictions, labels))
+        print(results)
 
-    trainer.train()
-
-    # SAVE MODEL
-
-    model.save_pretrained(save_dir)
-    tokenizer.save_pretrained(save_dir)
-
-    ### EVALUATE
-
-    trainer.evaluate()
-
-    predictions, labels, _ = trainer.predict(test_encodings)
-
-    preds = predictions.argmax(-1)
-    results = compute_metrics((predictions, labels))
-    print(results)
+if __name__== "__main__":
+    log = logging.getLogger(__name__)
+    main()
